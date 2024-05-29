54688f30cbe24bb81055037ea62aaee0c4fb9c60
Sakthivel J <sakthivelgreen.sk@gmail.com>
2024-05-30 04:20:54 +0530

    KVCET-Project

---
+++ Packages.py
@@ -0,0 +1,29 @@
+import subprocess
+import time
+pip_commands=[
+    "pip list",
+    "python -m pip install --upgrade pip",
+    "pip install seaborn",
+    "pip install plotly",
+    "pip install imagehash",
+    "pip install opencv-python",
+    "pip install flask",
+    "pip install mysql-connector",
+    "pip install cryptography",
+    "pip install scikit-learn",
+    "pip install scikit-image",
+    "pip install scpay",
+    "pip install flash_mail",
+    "pip install wordcloud",
+    "pip install gensim==3.7.1",
+
+    "pip list",
+
+    ]
+
+for commands in pip_commands:
+    if commands==pip_commands[0]:
+        print("==============| YOUR PREVIOUS PIP LIST |=============")
+    elif commands==pip_commands[7]:
+        print("==========| YOUR INSTALLED PIP LIST  |===========")
+    subprocess.run(commands,shell=True)
